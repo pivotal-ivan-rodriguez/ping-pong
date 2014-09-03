5d979a361c9572c46953f314e985ec7c574ef57b
CannaW/DomenicC <cwen@pivotallabs.comW>
2014-09-03 10:28:56 -0400

    Initial Commit

---
+++ PingPong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by DX169-XL on 2014-09-03.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PingPong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PingPong
+//
+//  Created by DX169-XL on 2014-09-03.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
